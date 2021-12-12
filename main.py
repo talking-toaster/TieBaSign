85f022052434797a1dd8e0488e9094d36c851b08
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-12-12 15:32:23 +0800

    update

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def main():
         if len(i) <= 0:
             logger.info("未检测到BDUSS")
             continue
-        logger.info("开始签到第" + str(n) + "个用户" + i)
+        logger.info("开始签到第" + str(n) + "个用户")
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         global favorites_num
